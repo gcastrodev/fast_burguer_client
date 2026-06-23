3f4fe523dd628a05afb3817a06837e0ef6430402
Gabriel Castro <castrovisionario@icloud.com>
2026-06-23 17:54:08 -0300

    feat: add global styles and continue login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Login } from './pages/Login';
+import GlobalStyles from './styles/GlobalStyles';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
+    <GlobalStyles />
   </StrictMode>,
 );
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,39 @@
+import Logo from '../../assets/fastburguer_logo.png';
+import {
+  Button,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Title,
+} from './styles';
+
 export function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo_fastburguer" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Seja bem-vindo ao <span> Fast Burguer!</span>
+          <br /> Entre com sua conta para
+          <span> começar a pedir.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label htmlFor="">E-mail</label>
+            <input type="text" />
+            <label htmlFor="">Senha</label>
+            <input type="password" name="" id="" />
+          </InputContainer>
+          <Button>Entrar</Button>
+        </Form>
+        <p>
+          Não possui conta? <a href="">Clique aqui.</a>
+        </p>
+      </RightContainer>
+    </Container>
   );
 }
